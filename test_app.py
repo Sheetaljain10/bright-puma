56fd359645a58260d0ea5abd0b98f41222a46ca3
Sheetal Jain <sheetal.is21@bmsce.ac.in>
2025-02-06 11:20:53 +0530

    Fixed host binding issue4

--- test_app.py
+++ test_app.py
@@ -1,4 +1,5 @@
 import unittest
+from flask import Flask, jsonify
 from app import app
 
 class TestApp(unittest.TestCase):
@@ -7,7 +8,7 @@ class TestApp(unittest.TestCase):
         response = tester.get("/")
         print(response.data.decode("utf-8"))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode("utf-8"), "Hello, Jenkins Multi-Stage Pipeline!")
+        self.assertEqual(response.data.decode("utf-8"), jsonify({"state": "Healthy"}))
 
 if __name__ == "__main__":
     unittest.main()
